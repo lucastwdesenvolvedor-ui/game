6842d4bd75b3c9327dc9d14b128438b87df3209f
Lucas <lucas.t.w.desenvolvedor@gmail.com>
2025-12-23 21:32:31 -0300

    final

--- server.js
+++ server.js
@@ -37,6 +37,5 @@ io.on("connection", socket => {
 
 });
 
-http.listen(3000, () => {
-  console.log("Servidor rodando em http://localhost:3000");
-});
+const PORT = process.env.PORT || 3000;
+app.listen(PORT);
